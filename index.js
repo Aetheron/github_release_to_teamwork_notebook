d1983e0880c1b7c5444ea5b2c9882bf7d48f6117
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2021-05-20 15:49:02 -0400

    Use existing contents and append new content

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const github = require('@actions/github');
 const endpoint = "/projects/api/v3/notebooks/"
 let url = "https://" + core.getInput('domain') + endpoint + core.getInput('notebook_id') + '.json';
 
+// The existing contents of the notebook
+let contents = '';
+
 // Send GET request to get the current contents of the notebook
 const getOpts = {
   method: 'GET',
@@ -18,13 +21,16 @@ const getOpts = {
 
 fetch(url, getOpts)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data.notebook.contents);
+    contents = data.notebook.contents;
+  });
 
 // Send PATCH request to append the new release tag to the notebook
 let data = `{
   "notebook": {
     "newVersion": true,
-    "contents": "
+    "contents": "${contents}
     <div><div><h1 data-ot-auto-id=\"true\" id=\"version-104-released-in-february-8-2021\"><span style=\"font-size: 20pt;\"><span style=\"color: rgb(0, 0, 0);\"><span style=\"background-color: transparent;\">Version ${github.context.ref} - Released on ${(new Date()).toDateString()}</span></span></span></h1>
     <table><tbody><tr><td style=\"border-color: rgb(0, 0, 0); border-style: solid; border-width: 1pt; padding: 5pt;\"><p><strong><span style=\"font-size: 11pt;\"><span style=\"color: rgb(0, 0, 0);\"><span style=\"background-color: transparent;\">Update</span></span></span></strong></p></td><td style=\"border-color: rgb(0, 0, 0); border-style: solid; border-width: 1pt; padding: 5pt;\"><p><strong><span style=\"font-size: 11pt;\"><span style=\"color: rgb(0, 0, 0);\"><span style=\"background-color: transparent;\">Author</span></span></span></strong></p></td></tr><tr><td style=\"border-color: rgb(0, 0, 0); border-style: solid; border-width: 1pt; padding: 5pt;\"><p><span style=\"font-size: 11pt;\"><span style=\"color: rgb(0, 0, 0);\"><span style=\"background-color: transparent;\">Fix in events not showing date of, addition of alert bar</span></span></span></p></td><td style=\"border-color: rgb(0, 0, 0); border-style: solid; border-width: 1pt; padding: 5pt;\"><p><span style=\"font-size: 11pt;\"><span style=\"color: rgb(0, 0, 0);\"><span style=\"background-color: transparent;\">Julie, Audrey, Bob</span></span></span></p></td></tr></tbody></table>",
   }
